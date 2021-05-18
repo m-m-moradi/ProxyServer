9f329e3d476fc01577ada2109bb6553048a1d6f0
Mahdi Moradi <mahdi.moradi.w@gmail.com>
2021-05-18 19:45:50 +0430

    before checking bufferdReader

--- src/main/java/HttpParser.java
+++ src/main/java/HttpParser.java
@@ -30,6 +30,7 @@ public class HttpParser {
     private ArrayList<String> headers;
     private ArrayList<Byte> body;
     private ArrayList<ArrayList<Byte>> chunked_body;
+    private ArrayList<Byte> original_data;
 
 
     public HttpParser(InputStream inputStream) {
@@ -42,6 +43,7 @@ public class HttpParser {
         StringBuilder lineBuilder = new StringBuilder();
         StringBuilder bodyBuilder = new StringBuilder();
         ArrayList<Character> array_list_char = new ArrayList<Character>();
+        ArrayList<Byte> original_data = new ArrayList<>();
 
 
         char c1, c2, c3, c4;
@@ -49,11 +51,15 @@ public class HttpParser {
 
         while (true) {
             while ((b1 = this.inputStream.read()) != '\r') {
-                if (b1 == -1) return -1;
+                if (b1 == -1) return -2;
                 c1 = (char) b1;
                 array_list_char.add(c1);
+                original_data.add((byte) b1);
 
             }
+
+            original_data.add((byte) '\r');
+            original_data.add((byte) '\n');
 
             b2 = this.inputStream.read(); // reading LF
 
@@ -122,6 +128,7 @@ public class HttpParser {
         // here must implement all proxy things
         boolean there_is_host = false;
         ArrayList<Integer> indices = new ArrayList<>();
+        int content_size = 0;
 
         for (String h : headers) {
             if (h.contains("Content-Length:")) {
@@ -130,15 +137,18 @@ public class HttpParser {
                 int byte_num = Integer.parseInt(byte_num_str.trim().strip());
                 int counter = 0;
                 while (counter != byte_num) {
-                    System.out.println(String.format("byte_num : %d , counter : %d , difference : %d", byte_num, counter, byte_num - counter));
+//                    System.out.println(String.format("byte_num : %d , counter : %d , difference : %d", byte_num, counter, byte_num - counter));
                     int difference = byte_num - counter;
                     byte[] chunk;
                     if (difference < 1024)
                         chunk = new byte[difference];
                     else
                         chunk = new byte[1024];
+
                     counter += this.inputStream.read(chunk, 0, chunk.length);
+
                     body.addAll(Arrays.asList(ArrayUtils.toObject(chunk)));
+                    original_data.addAll(Arrays.asList(ArrayUtils.toObject(chunk)));
                 }
             }
             if (h.contains("Transfer-Encoding:")) {
@@ -176,7 +186,12 @@ public class HttpParser {
 
                         while ((b = this.inputStream.read()) != '\r') {
                             hex_str_builder.append((char) b);
+                            original_data.add((byte) b);
                         }
+
+                        original_data.add((byte) '\r');
+                        original_data.add((byte) '\n');
+
                         this.inputStream.read(); // Consume the trailing '\n'
 
                         String hex_str = hex_str_builder.toString();
@@ -192,6 +207,9 @@ public class HttpParser {
 //
 //                            body.add((byte) '\r');
 //                            body.add((byte) '\n');
+
+                            original_data.add((byte) '\r');
+                            original_data.add((byte) '\n');
 
                             this.inputStream.read();
                             this.inputStream.read();
@@ -215,14 +233,21 @@ public class HttpParser {
                                 else
                                     little_chunk = new byte[1024];
                                 counter += this.inputStream.read(little_chunk, 0, little_chunk.length);
+                                content_size += little_chunk.length;
                                 real_chunk.addAll(Arrays.asList(ArrayUtils.toObject(little_chunk)));
+                                original_data.addAll(Arrays.asList(ArrayUtils.toObject(little_chunk)));
                             }
                             chunked_body.add(real_chunk);
+
+                            original_data.add((byte) '\r');
+                            original_data.add((byte) '\n');
 
                             this.inputStream.read();
                             this.inputStream.read();
                         }
                     }
+
+
                 }
             }
 
@@ -236,6 +261,10 @@ public class HttpParser {
 
                 if (h.contains("Host:"))
                     there_is_host = true;
+
+                if (h.contains("Transfer-Encoding:"))
+                    if (h.contains("chunked"))
+                        indices.add(headers.indexOf(h));
             }
         }
 
@@ -248,6 +277,10 @@ public class HttpParser {
                         headers.remove(x);
                 }
             }
+
+            if (isChunkEncoding) {
+                headers.add("Content-Length: " + content_size);
+            }
         }
 
         this.firstLinePart1 = firstPart;
@@ -259,6 +292,7 @@ public class HttpParser {
 
         this.body = body;
         this.chunked_body = chunked_body;
+        this.original_data = original_data;
 
         return 0;
     }
@@ -270,23 +304,28 @@ public class HttpParser {
             stringBuilder.append(h).append("\r\n");
         stringBuilder.append("\r\n");
         ArrayList<Byte> bytes = new ArrayList<>(Arrays.asList(ArrayUtils.toObject(stringBuilder.toString().getBytes(StandardCharsets.US_ASCII))));
+//        if (isChunkEncoding) {
+//            for (ArrayList<Byte> chunk : this.chunked_body) {
+//                int chunk_size = chunk.size();
+//                String s = Integer.toHexString(chunk_size);
+//                bytes.addAll(Arrays.asList(ArrayUtils.toObject(s.getBytes(StandardCharsets.US_ASCII))));
+//                bytes.add((byte) '\r');
+//                bytes.add((byte) '\n');
+//                bytes.addAll(chunk);
+//                bytes.add((byte) '\r');
+//                bytes.add((byte) '\n');
+//            }
+//            String s = Integer.toHexString(0);
+//            bytes.addAll(Arrays.asList(ArrayUtils.toObject(s.getBytes(StandardCharsets.US_ASCII))));
+//            bytes.add((byte) '\r');
+//            bytes.add((byte) '\n');
+//            bytes.add((byte) '\r');
+//            bytes.add((byte) '\n');
+
         if (isChunkEncoding) {
             for (ArrayList<Byte> chunk : this.chunked_body) {
-                int chunk_size = chunk.size();
-                String s = Integer.toHexString(chunk_size);
-                bytes.addAll(Arrays.asList(ArrayUtils.toObject(s.getBytes(StandardCharsets.US_ASCII))));
-                bytes.add((byte) '\r');
-                bytes.add((byte) '\n');
                 bytes.addAll(chunk);
-                bytes.add((byte) '\r');
-                bytes.add((byte) '\n');
             }
-            String s = Integer.toHexString(0);
-            bytes.addAll(Arrays.asList(ArrayUtils.toObject(s.getBytes(StandardCharsets.US_ASCII))));
-            bytes.add((byte) '\r');
-            bytes.add((byte) '\n');
-            bytes.add((byte) '\r');
-            bytes.add((byte) '\n');
         } else {
             if (this.body.size() != 0)
                 bytes.addAll(this.body);
@@ -363,5 +402,8 @@ public class HttpParser {
         return body;
     }
 
+    public ArrayList<Byte> getOriginal_data() {
+        return original_data;
+    }
 }
 
--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -5,7 +5,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -45,10 +44,12 @@ class RequestHandler extends Thread {
         this.log = new FileWriter(f);
     }
 
-    public void print(String message) throws IOException {
-        System.out.println(message);
+    public void print(String message, boolean terminal) throws IOException {
+        this.log.write("\n\n");
+        if (terminal)
+            System.out.println(message);
         this.log.write(message);
-        this.log.write("\n");
+        this.log.write("\n\n");
     }
 
     public void handleHTTPS(String remote_host, int remote_port) throws IOException {
@@ -61,8 +62,6 @@ class RequestHandler extends Thread {
         String successful_message = "HTTP/1.1 200 OK\r\n\r\n";
         byte[] bytes = successful_message.getBytes();
         this.to_client.write(bytes, 0, bytes.length);
-
-        this.print("sent successful message");
 
         // for reading from client and sending it to remote server asynchronously
         new Thread(() -> {
@@ -81,6 +80,7 @@ class RequestHandler extends Thread {
 ////                    this.print(new String(request, StandardCharsets.UTF_8));
 //                    to_remote_server.write(request, 0, request_bytes_read);
 //                }
+
             } catch (IOException e) {
                 System.out.println(this.connection_number);
                 e.printStackTrace();
@@ -94,7 +94,6 @@ class RequestHandler extends Thread {
             }
         }).start();
 
-        this.print("receiving");
         final byte[] reply = new byte[1024];
         int response_bytes_read;
         try {
@@ -117,15 +116,14 @@ class RequestHandler extends Thread {
 
     public void handleHTTP(String remote_host, int remote_port, HttpParser httpParser) throws IOException, InterruptedException {
 
-
-        ArrayList<Byte> monitoring = new ArrayList<>();
         Socket remote_socket = new Socket(remote_host, remote_port);
-        InputStream from_remote_server = remote_socket.getInputStream();
+        InputStream from_remote_server =  remote_socket.getInputStream();
         OutputStream to_remote_server = remote_socket.getOutputStream();
 
-
-        this.print("SENDING: --");
         ArrayList<Byte> message = httpParser.toBytes();
+
+        this.print(httpParser.makeString(), true);
+
         int chunk_size = 4096;
         for (int i = 0; i < message.size(); i += chunk_size) {
             List<Byte> sublist = message.subList(i, Math.min(i + chunk_size, message.size()));
@@ -134,184 +132,59 @@ class RequestHandler extends Thread {
         }
 
 
-        new Thread(() -> {
+        while (true) {
             try {
-                while (true) {
-
-
-                    HttpParser from_client_to_server_msg = new HttpParser(this.from_client);
-                    if (from_client_to_server_msg.readRequest() > 0) {
-
-//                    this.print("\n[[[Client to Server]]]");
-//                    this.print(from_client_to_server_msg.makeString());
-//                    this.print("\n");
-
-                        ArrayList<Byte> CS_message = from_client_to_server_msg.toBytes();
-                        for (int i = 0; i < CS_message.size(); i += chunk_size) {
-                            List<Byte> sublist = CS_message.subList(i, Math.min(i + chunk_size, CS_message.size()));
-                            byte[] bytes = ArrayUtils.toPrimitive(sublist.toArray(new Byte[0]));
-                            to_remote_server.write(bytes, 0, bytes.length);
-                        }
-                    }
-
-
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    this.log.close();
-                    this.socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-
-        Thread.sleep(2000);
-        try {
-            while (true) {
-//                HttpParser from_server_to_client_msg = new HttpParser(from_remote_server);
-//                from_server_to_client_msg.readRequest();
-
-//                this.print("\n[[[Server to Client]]]");
-//                this.print(from_server_to_client_msg.makeString());
-//                this.print("\n");
-
-
-//                message = from_server_to_client_msg.original();
-
-//                this.to_client.write(ArrayUtils.toPrimitive(message.toArray(new Byte[0])), 0, message.size());
-
-//                for (int i = 0; i < message.size(); i += chunk_size) {
-//                    List<Byte> sublist = message.subList(i, Math.min(i + chunk_size, message.size()));
-//                    byte[] bytes = ArrayUtils.toPrimitive(sublist.toArray(new Byte[0]));
-//                    this.to_client.write(bytes, 0, bytes.length);
-//                }
-
-//                byte[][] all = splitArray(ArrayUtils.toPrimitive(message.toArray(new Byte[0])) , message.size());
-//                for (byte[] record : all)
-//                    this.to_client.write(record, 0, record.length);
-                // ------------------
-
-
                 HttpParser from_server_to_client_msg = new HttpParser(from_remote_server);
-                from_server_to_client_msg.readRequest();
-//
-////                    this.print("\n[[[Server to Client]]]");
-////                    this.print(from_server_to_client_msg.makeString());
-////                    this.print("\n");
-//
-                ArrayList<Byte> SC_message = from_server_to_client_msg.toBytes();
-//
-                for (int i = 0; i < SC_message.size(); i += chunk_size) {
-                    List<Byte> sublist = SC_message.subList(i, Math.min(i + chunk_size, SC_message.size()));
-                    byte[] bytes = ArrayUtils.toPrimitive(sublist.toArray(new Byte[0]));
-                    System.out.println(bytes.length);
-                    this.to_client.write(bytes, 0, bytes.length);
+                if (from_server_to_client_msg.readRequest() != -2) {
+//                    ArrayList<Byte> SC_message = from_server_to_client_msg.toBytes();
+                    ArrayList<Byte> SC_message = from_server_to_client_msg.getOriginal_data();
+
+                    this.print(from_server_to_client_msg.makeString(), true);
+
+                    for (int i = 0; i < SC_message.size(); i += chunk_size) {
+                        List<Byte> sublist = SC_message.subList(i, Math.min(i + chunk_size, SC_message.size()));
+                        byte[] bytes = ArrayUtils.toPrimitive(sublist.toArray(new Byte[0]));
+//                    System.out.println(bytes.length);
+                        this.to_client.write(bytes, 0, bytes.length);
+                    }
                 }
-//
-//                    int[][] all = splitArray(ArrayUtils.toPrimitive(SC_message.toArray(new Integer[0])), 4096);
-//                    for (int[] record : all) {
-//                        System.out.println(record.length);
-////                        System.out.println(new String(record, StandardCharsets.US_ASCII));
-//                        this.to_client.write(record, 0, record.length);
-//                    }
-
-//                    for (int i=0 ; i< SC_message.size(); i++){
-//                        this.to_client.write(SC_message.get(i));
-//                    }
-
-//                    byte[] what = ArrayUtils.toPrimitive(SC_message.toArray(new Byte[0]));
-//                    this.to_client.write(what, 0, SC_message.size());
-
-                System.out.println("lkasdfl;jsdl;jf;lsadjf;sadjf;lksadjg;olaerhjsfg;lsdrhf;lkasdfhjf'l;hkjrsd");
+            } catch (Exception e) {
+                this.socket.close();
+                this.log.close();
+                e.printStackTrace();
                 break;
             }
-//                int b;
-//                char c;
-//                while ((b = from_remote_server.read()) != -1) {
-//                    c = (char) b;
-//                    this.to_client.write(b);
-//
-//                    monitoring.add((byte) c);
-//                }
 
-
-        } catch (IOException | URISyntaxException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
             try {
-                this.log.close();
+                HttpParser from_client_to_server_msg = new HttpParser(this.from_client);
+                if (from_client_to_server_msg.readRequest() != -2) {
+                    ArrayList<Byte> CS_message = from_client_to_server_msg.toBytes();
+
+                    this.print(from_client_to_server_msg.makeString(), true);
+
+                    for (int i = 0; i < CS_message.size(); i += chunk_size) {
+                        List<Byte> sublist = CS_message.subList(i, Math.min(i + chunk_size, CS_message.size()));
+                        byte[] bytes = ArrayUtils.toPrimitive(sublist.toArray(new Byte[0]));
+                        to_remote_server.write(bytes, 0, bytes.length);
+                    }
+                }
+            } catch (Exception e) {
                 this.socket.close();
-            } catch (IOException e) {
+                this.log.close();
                 e.printStackTrace();
+                break;
             }
         }
-    }
 
-
-    public static int[][] splitArray(int[] arrayToSplit, int chunkSize) {
-        if (chunkSize <= 0) {
-            return null;  // just in case :)
-        }
-        // first we have to check if the array can be split in multiple
-        // arrays of equal 'chunk' size
-        int rest = arrayToSplit.length % chunkSize;  // if rest>0 then our last array will have less elements than the others
-        // then we check in how many arrays we can split our input array
-        int chunks = arrayToSplit.length / chunkSize + (rest > 0 ? 1 : 0); // we may have to add an additional array for the 'rest'
-        // now we know how many arrays we need and create our result array
-        int[][] arrays = new int[chunks][];
-        // we create our resulting arrays by copying the corresponding
-        // part from the input array. If we have a rest (rest>0), then
-        // the last array will have less elements than the others. This
-        // needs to be handled separately, so we iterate 1 times less.
-        for (int i = 0; i < (rest > 0 ? chunks - 1 : chunks); i++) {
-            // this copies 'chunk' times 'chunkSize' elements into a new array
-            arrays[i] = Arrays.copyOfRange(arrayToSplit, i * chunkSize, i * chunkSize + chunkSize);
-        }
-        if (rest > 0) { // only when we have a rest
-            // we copy the remaining elements into the last chunk
-            arrays[chunks - 1] = Arrays.copyOfRange(arrayToSplit, (chunks - 1) * chunkSize, (chunks - 1) * chunkSize + rest);
-        }
-        return arrays; // that's it
-    }
-
-    public static byte[][] splitArray(byte[] arrayToSplit, int chunkSize) {
-        if (chunkSize <= 0) {
-            return null;  // just in case :)
-        }
-        // first we have to check if the array can be split in multiple
-        // arrays of equal 'chunk' size
-        int rest = arrayToSplit.length % chunkSize;  // if rest>0 then our last array will have less elements than the others
-        // then we check in how many arrays we can split our input array
-        int chunks = arrayToSplit.length / chunkSize + (rest > 0 ? 1 : 0); // we may have to add an additional array for the 'rest'
-        // now we know how many arrays we need and create our result array
-        byte[][] arrays = new byte[chunks][];
-        // we create our resulting arrays by copying the corresponding
-        // part from the input array. If we have a rest (rest>0), then
-        // the last array will have less elements than the others. This
-        // needs to be handled separately, so we iterate 1 times less.
-        for (int i = 0; i < (rest > 0 ? chunks - 1 : chunks); i++) {
-            // this copies 'chunk' times 'chunkSize' elements into a new array
-            arrays[i] = Arrays.copyOfRange(arrayToSplit, i * chunkSize, i * chunkSize + chunkSize);
-        }
-        if (rest > 0) { // only when we have a rest
-            // we copy the remaining elements into the last chunk
-            arrays[chunks - 1] = Arrays.copyOfRange(arrayToSplit, (chunks - 1) * chunkSize, (chunks - 1) * chunkSize + rest);
-        }
-        return arrays; // that's it
     }
 
     public void run() {
         try {
 
-            this.print(String.format("Connection number: %d", this.connection_number));
-            this.print(String.format("Socket: %s", this.socket));
+            this.print(String.format("Connection number: %d", this.connection_number), true);
+            this.print(String.format("Socket: %s", this.socket), true);
 
-            this.from_client = socket.getInputStream();
+            this.from_client =  socket.getInputStream();
             this.to_client = socket.getOutputStream();
 
             HttpParser httpParser = new HttpParser(this.from_client);
@@ -321,15 +194,15 @@ class RequestHandler extends Thread {
                 return;
             }
 
-            this.print(String.format("Main Line : %s", httpParser.getMainLine()));
-            this.print(String.format("headers : %s", httpParser.getHeaders()));
-            this.print(String.format("body : [%s]", httpParser.getBodyAsString()));
+            this.print(String.format("Main Line : %s", httpParser.getMainLine()), true);
+            this.print(String.format("headers : %s", httpParser.getHeaders()), true);
+            this.print(String.format("body : [%s]", httpParser.getBodyAsString()), true);
 
             // we are sure this is request
-            this.print(String.format("Method : %s", httpParser.getFirstLinePart1()));
-            this.print(String.format("Remote host: %s", httpParser.getHost()));
-            this.print(String.format("version : %s", httpParser.getFirstLinePart3()));
-            this.print(String.format("Remote port: %s", httpParser.getPort()));
+            this.print(String.format("Method : %s", httpParser.getFirstLinePart1()), true);
+            this.print(String.format("Remote host: %s", httpParser.getHost()), true);
+            this.print(String.format("version : %s", httpParser.getFirstLinePart3()), true);
+            this.print(String.format("Remote port: %s", httpParser.getPort()), true);
 
             if (httpParser.getFirstLinePart1().equals("CONNECT"))
                 this.handleHTTPS(httpParser.getHost(), httpParser.getPort());
